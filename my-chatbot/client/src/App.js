4898062373a2afa645023b406836c1013b02facd
martingotz <gotzm@udesa.edu.ar>
2024-10-29 21:04:20 -0300

    /

--- my-chatbot/client/src/App.js
+++ my-chatbot/client/src/App.js
@@ -29,6 +29,7 @@ function App() {
         <div className="App">
           <Header />
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/tesis14" element={<Home />} />
             <Route path="/inicio" element={<Home />} />
             <Route path="/login" element={<LoginForm />} />
